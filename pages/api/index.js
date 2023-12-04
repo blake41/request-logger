4ea7cec4cddc2b206f4be2ee4dfc56230e004b65
blake johnson <blake41@gmail.com>
2023-12-04 16:48:39 -0500

    should be working

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,28 +5,15 @@ export default function handler(req, res, next) {
   // res.setHeader('Content-Type', 'application/json');
   // console.log(Object.keys(req))
   // return res.status(200).json({ text:  });
-  async function getURI(url) {
-    console.log(`${url}`)
-    try {
-      const response = await axios.get(url);
-      if (response.status !== 200) {
-        console.log('non 200 response')
-        return res.status(response.status).json({ type: 'error', message: response.statusText });
-      } else {
-        console.log('200 response')
-        res.json(JSON.stringify(response.data))
-      }
-    } catch (error) {
-      console.log(error.message, "ERR")
-      return res.status(500).json({ type: 'error', message: error.message });
-    }
-  } 
-  console.log(`request was from ${req.url}`)
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(`${ip}`)
+  return res.status(200);
+} 
+
+console.log(`request was from ${req.url}`)
+const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+console.log(`${ip}`)
   // console.log(`${req.body}`)
   // console.log(`${Object.keys(JSON.parse(req.body))}`)
-  return getURI(JSON.parse(req.body)['my-url'])
+  // return getURI(JSON.parse(req.body)['my-url'])
   
   // return res.status(200)
   
